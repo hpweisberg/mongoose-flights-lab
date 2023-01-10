50ca794c8a6cb280f819386aab840eb3f566ccbe
hpweisberg <hpweisberg@gmail.com>
2023-01-09 17:06:18 -0800

    connect database and define flightSchema

---
+++ models/flight.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema
+
+const flightSchema = new Schema({
+  airline: {
+    type: String,
+    enum: ['American', 'Southwest', 'United', 'JetBlue']
+  },
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN', 'JFK'],
+    default: 'DEN',
+  },
+  flightNo: {
+    type: Number,
+    min: 10,
+    max: 9999,
+  },
+  departs: {
+    type: Date,
+    default: function(){
+      return new Date().getFullYear() + 1
+    }
+  }
+})
