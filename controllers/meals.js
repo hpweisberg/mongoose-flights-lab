c528d492146354ee008faa820239d85abe415535
hpweisberg <hpweisberg@gmail.com>
2023-01-11 16:33:29 -0800

    add the ability to post new meals

--- controllers/meals.js
+++ controllers/meals.js
@@ -14,8 +14,18 @@ function newMeal(req, res){
   })
 }
 
+function create(req, res){
+  Meal.create(req.body)
+  .then(meal => {
+    res.redirect('/meals/new')
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/meals/new')
+  })
+}
 
 export {
   newMeal as new,
-
+  create,
 }
